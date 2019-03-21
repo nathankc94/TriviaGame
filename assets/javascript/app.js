8d4f9ea995a3c6df37e565d4f1b702747313c25a
alongkot <nathankc94@gmail.com>
2019-03-21 14:09:42 -0500

    creating var for questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,105 @@
+
+
+
+
+// create global objects
+var timeRemaining = 0;
+var questions = [
+    {question: "A person who’s a fysigunkus lacks what?",
+options:["Humor", "Wisdom", "Curiosity", "Temper"],
+answer: "Curiosity",
+image: "assets/images/"
+},
+    {question: "What does psithurism describe the sound of?",
+options:["Flowing water", "Rustling leaves", "Keyboard typing", "Hammer nailing"],
+answer: "Rustling leaves",
+image: "assets/images/"
+},
+    {question: "What is a wheeple?",
+options:["A poor attempt at whistling", "A poor attempt at listening", "A poor attempt at sneezing", "A poor attempt at hugging"],
+answer: "A poor attempt at whistling",
+image: "assets/images/"
+},
+    {question: "What’s limerance the initial thrill of?",
+options:["Getting a job", "Falling in love", "Learning to write", "Buying a house"],
+answer: "Falling in love",
+image: "assets/images/"
+},
+    {question: "What’s another word for chirotonsor?",
+options:["A masseur", "A carpenter", "A barber", "A dentist"],
+answer: "A barber",
+image: "assets/images/"
+},
+    {question: "What do you love eating as a pagophagiac",
+options:["Fingernails", "Ash", "Pips", "Ice"],
+answer: "Ice",
+image: "assets/images/"
+},
+    {question: "When you’re a stagiary, what are you a student of?",
+options:["Medicine", "Law", "Geology", "Philosophy"],
+answer: "Law",
+image: "assets/images/"
+},
+    {question: "Presbycusis is the loss of what at old age?",
+options:["Smelling", "Hearing", "Tasting", "Feeling"],
+answer: "Hearing",
+image: "assets/images/"
+}  
+];
+
+var questionOption = 
+
+var correctAnswers = 0;
+var wrongAnswer = 0;
+var unAnswered = 0;
+
+
+
+// function
+
+// start game
+
+// timer to count down
+
+// user input (clicking)
+
+// start over function 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// main processor 
+
+//  disply html
+// disply timer
+// disply questions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
